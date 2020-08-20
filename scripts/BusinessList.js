b28f085feff0a68e613277a45ba8ff8186b837fa
Ember Parr <ember-devops@gmail.com>
2020-08-20 17:59:44 -0500

    added filtered list containing only new york companies

--- scripts/BusinessList.js
+++ scripts/BusinessList.js
@@ -1,6 +1,12 @@
 import { useBusinessList } from './BusinessProvider.js';
 import { Business } from './Business.js';
 
+import { newYorkBusinesses } from './BusinessProvider.js';
+import { nyBusiness } from './Business.js';
+
+
+
+// runs through the loop of all businesses
 const contentSelector = document.querySelector(".businessList");
 
 export const BusinessList = () => {
@@ -10,6 +16,21 @@ export const BusinessList = () => {
     businessArray.forEach(
         (businessObj) => {
             contentSelector.innerHTML += Business(businessObj)
+        }
+    );
+}
+
+
+// runs through the loop of all businesses FOR NEW YORK
+const contentSelectorTwo = document.querySelector(".businessList--newYork");
+
+export const nyBusinessList = () => {
+    const businessArray = newYorkBusinesses
+    contentSelectorTwo.innerHTML = "<h1>Only New York Sales</h1>"
+
+    businessArray.forEach(
+        (businessObj) => {
+            contentSelectorTwo.innerHTML += nyBusiness(businessObj)
         }
     );
 }
