abd49ff20a6d5aed623d30686aaab591bd29f69c
Ember Parr <ember-devops@gmail.com>
2020-08-26 11:39:06 -0500

    functioning search bar added

--- scripts/Business.js
+++ scripts/Business.js
@@ -1,23 +1,25 @@
 export const Business = (businessObject) => {
     return `
-    
+    <div class = "search">
         <h2 class="business-name">${businessObject.companyName}</h2>
         <div class="business-info">
             ${businessObject.addressFullStreet} <br>
             ${businessObject.addressCity}, ${businessObject.addressStateCode} ${businessObject.addressZipCode}
             <hr>
         </div>
-    
+        </div>
     `
 };
 
 export const nyBusiness = (nyBusinessObject) => {
     return `
+    <div class = "search">
         <h2 class="business-name">${nyBusinessObject.companyName}</h2>
         <div class="business-info">
             ${nyBusinessObject.addressFullStreet} <br>
             ${nyBusinessObject.addressCity}, ${nyBusinessObject.addressStateCode} ${nyBusinessObject.addressZipCode}
             <hr>
+        </div>
         </div>
     `
 }
@@ -25,11 +27,13 @@ export const nyBusiness = (nyBusinessObject) => {
 
 export const Agents = (agentsObject) => {
     return `
+    <div class = "search">
         <h2 class="agent-name">${agentsObject.agentName.nameFirst} ${agentsObject.agentName.nameLast}</h2>
         <div class="agent-info">
             ${agentsObject.agentCompany} <br>
             ${agentsObject.agentPhone}
             <hr>
+        </div>
         </div>
     `
 }
